393acc747ae0ec42c9084fb561471abe92e7ac76
loic.ngou <loic.ngou@1menu.me>
2019-11-07 22:28:22 +0100

    feat(dial) launch dial on phone number clicked

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -17,7 +17,7 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
-        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        ImageView candyStoreImageView = (ImageView) findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
@@ -25,13 +25,20 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    public void createMapIntent(View view){
+    public void createMapIntent(View view) {
         Uri adressUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW,adressUri);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, adressUri);
         mapIntent.setPackage("com.google.android.apps.maps");
-        if(mapIntent.resolveActivity(getPackageManager()) != null){
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
         }
+    }
+
+    public void createPhoneIntent(View view) {
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(phoneUri);
+        startActivity(phoneIntent);
     }
 
     // ***
